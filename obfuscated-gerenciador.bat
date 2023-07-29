7d8be319046cd46d5c313eadac757d442e606c33
thiaguindograu1234 <118500503+thiaguindograu1234@users.noreply.github.com>
2023-07-29 14:18:37 -0300

    Create obfuscated-gerenciador.bat

---
+++ obfuscated-gerenciador.bat
@@ -0,0 +1,98 @@
+rem obfuscado por porinha obfuscator
+%random ~123%%random ~456%%random ~789%%random ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%andom ~101112%%random ~131415%%random ~161718%
+%random ~987%%ranandom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%randodom ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+andom ~101112%%random ~13andom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%rando1415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~31andom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%rando4%%random ~159%%random ~265%%random ~358%%random ~979%
+%randoo1%%random ~246810%%random ~1357911%%random ~1211109876%andom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%randoandom ~101112%%random ~131415%%random ~161718%andom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%rando
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%v
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%randoandom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%randoandom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%andom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%rando%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~26andom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%rando5%%random ~358%%random ~979%
+%randoandom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%rando
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%randoandom ~101112%%random ~131415%%random ~161718%https://www.youtube.com/watch?v=TtYEUuYATro
+%random ~987%%random ~654%%random ~3andom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%andom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%rando
+%rando21%%random ~246810%%random ~1357911%%random ~1211109876%andom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%rando
+%random ~42%%random ~314%%random ~159%%random ~265%andom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~3andom ~101112%%random ~andom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%rando131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%andom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%randoandom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%randoandom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%randoandom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%rando
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%rando14%%randomandom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random andom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%randorandom ~159%%random ~265%%random ~358%%random ~979%
+%randoandom ~101112%%random ~131415%%random ~161718%andom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%rando
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%randoandom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%rando
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%random ~777%%random ~888%%random ~999%%random ~111%%random ~222%%random ~333%andom ~101112%%raandom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%randoandom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%randoandom ~101112%%random ~131415%%random ~161718%
+%random ~987%%random ~654%%random ~321%%random ~246810%%random ~1357911%%random ~1211109876%
+%random ~42%%random ~314%%random ~159%%random ~265%%random ~358%%random ~979%
+%rando
